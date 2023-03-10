a6c8bc780535f2b57d160bfa3e40c2749bf365f0
salomon eslait <salomoneslait@gmail.com>
2023-03-10 10:54:35 -0500

    update vpc file

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.3"
 
-  name = "main"
+  name = "devco-networking"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
